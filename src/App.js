5705c8f3efb5b7048a924e01533487edd863bb5d
Sepler Gábor <sepler.gabor@gmail.com>
2017-07-07 16:21:39 +0200

    That is all for today

--- src/App.js
+++ src/App.js
@@ -10,55 +10,224 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      usersData: []
+      usersData: {
+        page: {
+          content: []
+        },
+        links: {}
+      },
+      spinTimeData: {
+        page: {
+          content: []
+        },
+        links: {}
+      },
+      noSpinsData: {
+        page: {
+          content: []
+        },
+        links: {}
+      },
+      maxRpmsData: {
+        page: {
+          content: []
+        },
+        links: {}
+      },
+      usersVisible: true,
+      spinTimeVisible: false,
+      noSpinsVisible: false,
+      maxRpmsVisible: false,
     };
-    axios.get(root + '/api/best_times')
+    axios.get(root + '/api/bests?properties=time&page=0')
       .then((res) => {
         console.log("User data: ",res);
         this.setState({
-          usersData: res.data.page.content
+          usersData: res.data
         })
-
       })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+
+    axios.get(root + '/api/bests?properties=spinTime&page=0')
+      .then((res) => {
+        console.log("BEst spin times: ",res);
+        this.setState({
+          spinTimeData: res.data
+        })
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+
+    axios.get(root + '/api/bests?properties=numberOfSpins&page=0')
+      .then((res) => {
+        console.log("Best number of spins: ",res);
+        this.setState({
+          noSpinsData: res.data
+        })
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+
+    axios.get(root + '/api/bests?properties=maxRpm&page=0')
+      .then((res) => {
+        console.log("Best RPMs: ",res);
+        this.setState({
+          maxRpmsData: res.data
+        })
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
   }
 
-  mainRender() {
+  buttonRender() {
     return (
       <div className="buttons">
-        <button onClick={this.showBestTimes} className="timesButton">Best times</button>
-        <button onClick={this.showBestNoSpins} className="spinsButton">Best spins</button>
-        <button onClick={this.showBestMaxRpms} className="rpmsButton">Best RPMs</button>
+        <button onClick={this.showBestTimes.bind(this)} className="timesButton">Best times</button>
+        <button onClick={this.showBestNoSpins.bind(this)} className="spinsButton">Best spins</button>
+        <button onClick={this.showBestMaxRpms.bind(this)} className="rpmsButton">Best RPMs</button>
       </div>
     )
   }
 
-  showUsers() {
+  showBestTimes() {
+    this.setState({
+      usersVisible: false,
+      spinTimeVisible: true,
+      noSpinsVisible: false,
+      maxRpmsVisible: false
+    })
+  }
+
+  showBestNoSpins() {
+    this.setState({
+      usersVisible: false,
+      spinTimeVisible: false,
+      noSpinsVisible: true,
+      maxRpmsVisible: false
+    })
+  }
+
+  showBestMaxRpms() {
+    this.setState({
+      usersVisible: false,
+      spinTimeVisible: false,
+      noSpinsVisible: false,
+      maxRpmsVisible: true
+    })
+  }
+
+  showTables() {
     let cols = [
         { key: 'nickName', label: 'User name' },
         { key: 'spinTime', label: 'Spin time' },
         { key: 'numberOfSpins', label: 'No of spins' },
         { key: 'maxRpm', label: 'Max RPM' }
     ];
+    let cols1 = [
+        { key: 'nickName', label: 'User name' },
+        { key: 'spinTime', label: 'Spin time' },
+    ];
+    let cols2 = [
+        { key: 'nickName', label: 'User name' },
+        { key: 'numberOfSpins', label: 'No of spins' },
+    ];
+    let cols3 = [
+        { key: 'nickName', label: 'User name' },
+        { key: 'maxRpm', label: 'Max RPM' }
+    ];
     console.log('Users data: ', this.state.usersData);
     return <div>
-      <Table cols={cols} data={this.state.usersData}/>
+      <Table cols={cols} data={this.state.usersData.page.content} display={this.state.usersVisible} prev={this.state.usersData.links.prev} next={this.state.usersData.links.next} set='usersData'/>
+      <Table cols={cols1} data={this.state.spinTimeData.page.content} display={this.state.spinTimeVisible} prev={this.state.spinTimeData.links.prev} next={this.state.spinTimeData.links.next} set='spinTimeData'/>
+      <Table cols={cols2} data={this.state.noSpinsData.page.content} display={this.state.noSpinsVisible} prev={this.state.noSpinsData.links.prev} next={this.state.noSpinsData.links.next} set='noSpinsData'/>
+      <Table cols={cols3} data={this.state.maxRpmsData.page.content} display={this.state.maxRpmsVisible} prev={this.state.maxRpmsData.links.prev} next={this.state.maxRpmsData.links.next} set='maxRpmsData'/>
     </div>
   }
+
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Fidget spinner leaderboard</h2>
+          <h2>Fidget<br/>spinner<br/>leaderboard</h2>
         </div>
         <p className="App-intro">
           <span>Play & <br/> challenge others to spin!</span>
         </p>
         <section className="mainTable">
-          {this.mainRender()}
-          {this.showUsers()}
+          {this.buttonRender()}
+          {this.showTables()}
         </section>
+        <div className="loginArea">
+          <img className="change" src={require("./arrows0.png")} alt=""/>
+          <p><span>LOGIN</span> or <span>REGISTER</span></p>
+        </div>
       </div>
     );
   }
--- src/Table.js
+++ src/Table.js
@@ -3,21 +3,28 @@ import React from "react";
 export class Table extends React.Component {
 
   render() {
-    let headerComponents = this.generateHeaders(),
-    rowComponents = this.generateRows();
-
-    return (
-      <div>
-        <table className="tables">
-          <thead>
-            <tr>
-            {headerComponents}
-            </tr>
-          </thead>
-          <tbody>{rowComponents}</tbody>
-        </table>
-      </div>
-    );
+    if (this.props.display === true) {
+      let headerComponents = this.generateHeaders(),
+      rowComponents = this.generateRows();
+      return (
+        <div>
+          <table className="tables">
+            <thead>
+              <tr>
+                {headerComponents}
+              </tr>
+            </thead>
+            <tbody>
+              {rowComponents}
+            </tbody>
+          </table>
+          <a key={'previous'} href={this.props.prev}><img className="arrowleft" src={require("./leftarrow.png")} alt=""/></a>
+          <a key={'next'} href={this.props.next}><img className="arrowright" src={require("./rightarrow.png")} alt=""/></a>
+        </div>
+      );
+    } else {
+      return null
+    }
   }
 
   generateHeaders() {
@@ -36,10 +43,10 @@ export class Table extends React.Component {
       // handle the column data within each row
       let cells = cols.map(function(colData) {
         if (colData.key === 'nickName') {
-          return <div className="container">
-            <td key={item[colData.key]}>{item[colData.key]}</td>
+          return <td key={item[colData.key]} className="container">
             <img key={'avatar ' + item[colData.key]} className="avatarImg" src={item.avatar} alt="" />
-          </div>
+            {item[colData.key]}
+          </td>
         }
         return <td key={item[colData.key]}>{item[colData.key]}</td>;
       });
